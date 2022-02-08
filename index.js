3f83f471740bdc4607605f373a23f2b2d5601705
Hectorjte <hectorjte@gmail.com>
2022-02-07 18:32:48 -0600

    Added linters

--- index.js
+++ index.js
@@ -7,75 +7,92 @@ class book {
 
 let BookList = [];
 
-const bookTitle = document.getElementById('bookTilte');
-const bookAuthor = document.getElementById('bookAuthor');
-const btnAdd = document.getElementById('Add');
+const bookTitle = document.getElementById("bookTilte");
+const bookAuthor = document.getElementById("bookAuthor");
+const btnAdd = document.getElementById("Add");
 
 function AddBook() {
-  if (bookTitle.value != '' && bookAuthor.value != '') {
+  if (bookTitle.value != "" && bookAuthor.value != "") {
     BookList.push(new book(bookTitle.value, bookAuthor.value));
+    localStorage.setItem("BookList", JSON.stringify(BookList));
+    location.reload();
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  btnAdd.addEventListener('click', AddBook);
+function RemoveBook() {
+  const index = this.id;;
+  if (index > -1) {
+    BookList.splice(index, 1);
+    localStorage.setItem("BookList", JSON.stringify(BookList));
+    location.reload();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  btnAdd.addEventListener("click", AddBook);
+  if (localStorage.getItem("BookList") != null) {
+    BookList = JSON.parse(localStorage.getItem("BookList"));
+    displayBooks();
+  }
 });
 
 function displayBooks() {
   if (BookList.length >= 1) {
-    BookList.map((el) => {
-      const BookSec = document.getElementById('bookSec');
-      const section = document.createElement('section');
+    BookList.map((el, i) => {
+      const BookSec = document.getElementById("bookSec");
+      const section = document.createElement("section");
 
-      const rowTitle = document.createElement('div');
-      rowTitle.classList.add('row');
-      rowTitle.classList.add('m-2');
+      const rowTitle = document.createElement("div");
+      rowTitle.classList.add("row");
+      rowTitle.classList.add("m-2");
 
-      const colTitle = document.createElement('div');
-      colTitle.classList.add('col-4');
-      colTitle.classList.add('m-2');
+      const colTitle = document.createElement("div");
+      colTitle.classList.add("col-4");
+      colTitle.classList.add("m-2");
 
-      const labelTitle = document.createElement('label');
-      labelTitle.classList.add('titleBook');
+      const labelTitle = document.createElement("label");
+      labelTitle.classList.add("titleBook");
       labelTitle.textContent = el.Title;
 
       colTitle.appendChild(labelTitle);
       rowTitle.appendChild(colTitle);
       section.appendChild(rowTitle);
 
-      const rowAuthor = document.createElement('div');
-      rowAuthor.classList.add('row');
-      rowAuthor.classList.add('m-2');
+      const rowAuthor = document.createElement("div");
+      rowAuthor.classList.add("row");
+      rowAuthor.classList.add("m-2");
 
-      const colAuthor = document.createElement('div');
-      colAuthor.classList.add('col-4');
-      colAuthor.classList.add('m-2');
+      const colAuthor = document.createElement("div");
+      colAuthor.classList.add("col-4");
+      colAuthor.classList.add("m-2");
 
-      const labelAuthor = document.createElement('label');
-      labelAuthor.classList.add('titleBook');
+      const labelAuthor = document.createElement("label");
+      labelAuthor.classList.add("titleBook");
       labelAuthor.textContent = el.Author;
 
       colAuthor.appendChild(labelAuthor);
       rowAuthor.appendChild(colAuthor);
       section.appendChild(rowAuthor);
 
-      const rowbtn = document.createElement('div');
-      rowbtn.classList.add('row');
-      rowbtn.classList.add('m-2');
+      const rowbtn = document.createElement("div");
+      rowbtn.classList.add("row");
+      rowbtn.classList.add("m-2");
 
-      const colbtn = document.createElement('div');
-      colbtn.classList.add('col-4');
+      const colbtn = document.createElement("div");
+      colbtn.classList.add("col-4");
 
-      const removeBtn = document.createElement('input');
-      removeBtn.type = 'submit';
-      removeBtn.id = 'Remove';
-      removeBtn.value = 'Remove';
+      const removeBtn = document.createElement("input");
+      removeBtn.type = "submit";
+      removeBtn.id = i.toString();
+      removeBtn.value = "Remove";
+      removeBtn.addEventListener("click", RemoveBook);
+
 
       colbtn.appendChild(removeBtn);
       rowbtn.appendChild(colbtn);
       section.appendChild(rowbtn);
 
-      const hr = document.createElement('hr');
+      const hr = document.createElement("hr");
       section.appendChild(hr);
       BookSec.appendChild(section);
 
@@ -83,46 +100,3 @@ function displayBooks() {
     });
   }
 }
-
-/* Storage local for data */
-var datas = ['1', '2', '3'];
-localStorage['datas'] = JSON.stringify(datas);
-
-// Retrieve
-var stored_datas = JSON.parse(localStorage['datas']);
-const formModel = {
-  name: 'Karam',
-  email: '',
-};
-const keymData = localStorage.getItem('formDataInput');
-if (keymData) {
-  const formDataInput = JSON.parse(keymData);
-  nameInput.value = formDataInput.name;
-  emailInput.value = formDataInput.email;
-  messageInput.value = formDataInput.msg;
-} else {
-  localStorage.setItem('formDataInput', JSON.stringify(formModel));
-}
-
-const dataStorage = (key, value) => {
-  const formDataInput = JSON.parse(localStorage.getItem('formDataInput'));
-  formDataInput[`${key}`] = value;
-  localStorage.setItem('formDataInput', JSON.stringify(formDataInput));
-};
-
-form.addEventListener('input', (e) => {
-  dataStorage(e.target.id, e.target.value);
-  switch (e.target.id) {
-    case 'name':
-      validInput();
-      break;
-    case 'email':
-      validInput();
-      break;
-    case 'msg':
-      validInput();
-      break;
-    default:
-      validInput();
-  }
-});
